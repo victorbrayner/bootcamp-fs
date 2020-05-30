8bb3bdc4bb0f6326ae08cef5626ec97da65d64e7
Victor Brayner <victorbrayner@gmail.com>
2020-05-29 22:06:36 -0300

    first column development

---
+++ modulo-1/country-checker/README.md
@@ -0,0 +1,20 @@
+<p align="center">
+  <img src="../assets/bootcamp-logo.png">
+</p>
+
+<h1 align="center">Country Checker</h1>
+<p align="justify">
+Mini-aplicação funcional escrita em JavaScript vanilla. Existem duas listas: uma percorrendo os 250 países cadastrados na API e outra que armazena os países que forem marcados com o + do botão da primeira lista, sendo assim uma lista de favoritos/marcados/viajados, etc.</p>
+
+<h2>Características:</h2>
+<ul>
+<li>Consumo de dados via API;</li>
+<li>Formatação e demonstração dos dados;</li>
+<li>Estilização via Materialize;</li>
+<li>Formação de lista.</li>
+</ul>
+
+## Desenvolvimento:
+- [x] Código funcional;
+- [ ] Layout estilizado;
+- [x] Boas práticas de JS;
---
+++ modulo-1/country-checker/js/script.js
@@ -0,0 +1,92 @@
+let tabCountries = null;
+let tabFavorites = null;
+
+let allCountries = [];
+let favCountries = [];
+
+let countCountries = 0;
+let countFavorites = 0;
+
+let totalPopulationList = 0;
+let totalPopulationFav = 0;
+
+let numberFormat = null;
+
+window.addEventListener('load', () => {
+    tabCountries = document.querySelector('#tabCountries');
+    tabFavorites = document.querySelector('#tabFavorites');
+    countCountries = document.querySelector('#countCountries');
+    countFavorites = document.querySelector('#countFavorites');
+    
+    totalPopulationList = document.querySelector('#totalPopulationList');
+    totalPopulationFav = document.querySelector('#totalPopulationFavorites');
+
+    numberFormat = Intl.NumberFormat('pt-BR');
+
+    fetchCountries();
+})
+
+async function fetchCountries(){
+    const res = await fetch('https://restcountries.eu/rest/v2/all');
+    const json = await res.json();
+
+    allCountries = json.map(country => {
+
+        const {id, translations, population, flag} = country
+
+        return {
+            id: country.numericCode,
+            name: country.translations.pt,
+            population: country.population,
+            flag: country.flag
+        }
+    });
+    render();
+}
+
+function render(){
+    renderCountryList();
+    renderFavorites();
+    renderSummary();
+    handleCountryButtons();
+}
+
+function renderCountryList(){
+    let countriesHTML = '<div>';
+
+    allCountries.forEach((country) => {
+        const {name, flag, id, population} = country;
+        const countryHTML = `
+        <div class='country'>
+            <div>
+                <a id="${id}" class="waves-effect waves-light btn">+</a>
+            </div>                
+            <div>
+                <img src="${flag}" alt="${name}">
+            </div>
+            <div>
+                <ul>
+                    <li>${name}</li>
+                    <li>${population}</li>
+            </div>
+        </div>
+        `;
+
+        countriesHTML += countryHTML;
+    });
+
+    tabCountries.innerHTML = countriesHTML;
+
+}
+
+function renderFavorites(){
+
+}
+
+function renderSummary(){
+
+}
+
+function handleCountryButtons(){
+
+}
