e64a9550d262af5dc960e18fe67cfaac2b3fff42
Victor Brayner <victorbrayner@gmail.com>
2020-06-05 17:22:04 -0300

    add async function howManyCities

--- modulo-2/states-cities-br-parser/index.js
+++ modulo-2/states-cities-br-parser/index.js
@@ -1,70 +1,48 @@
 import { promises } from "fs";
 const { readFile, writeFile } = promises;
 
-readWrite();
 
-async function readWrite(){
+let dataCities;
+let dataStates;
+
+async function init(){
     try {
-        const rawDataCities = await readFile("./cities.json");
-        const dataCities = JSON.parse(rawDataCities);
-        const rawDataStates = await readFile("./states.json");
-        const dataStates = JSON.parse(rawDataStates);
+        const readWrite = async () => {
+            const rawDataCities = await readFile("./cities.json");
+            dataCities = JSON.parse(rawDataCities);
+            const rawDataStates = await readFile("./states.json");
+            dataStates = JSON.parse(rawDataStates);
+            
+            dataStates.forEach(state => {
+                const citiesAndState = dataCities.filter(city => city.Estado === state.ID);
+                writeFile(`files/${state.Sigla}.json`, JSON.stringify(citiesAndState));
+            });
+            
+            howManyCities();
+        }
+        
+        const lookCities = async (state) => {
+            const cityJSON = JSON.parse(await readFile (`files/${state}.json`));
+            return cityJSON.length;
+            }
+            
+        const howManyCities = async () => {
+            const howManyList = [];
+            await Promise.all(dataStates.map(async (states)=>{
+                let howMany = await lookCities(states.Sigla);
+                let formattedState = {Sigla: states.Sigla, Quantidade: howMany}; 
+                            
+                howManyList.push(formattedState);
+                }));      
+                    
+            console.log(howManyList); 
+            }
 
-        dataStates.forEach(state => {
-            const citiesAndState = dataCities.filter(city => city.Estado === state.ID);
-            writeFile(`files/${state.Sigla}.json`, JSON.stringify(citiesAndState));
-        });
-
-
+        readWrite();
+        
     } catch (err) {
         console.log(err);
     }
 }
 
-/* const fs = require('fs');
-
-let rawDataStates = fs.readFileSync('states.json');
-let states = JSON.parse(rawDataStates);
-
-let rawDataCities = fs.readFileSync('cities.json');
-let cities = JSON.parse(rawDataCities);
-
-
-function howManyCities(UF){
-    let stateID = null;
-    let cityCounter = 0;
-    
-    function lookCities(stateID){
-        for (let i = 0; i < cities.length; i++) {
-            if (cities[i].Estado === stateID) {
-                cityCounter++;
-            }
-        }
-        return cityCounter;
-    }
-
-
-    for (let i = 0; i < states.length; i++) {
-        if (states[i].Sigla === UF) {
-            stateID = states[i].ID;
-            return lookCities(stateID);
-        }
-    }
-}
-
-function mostCities(){
-    const cityCounterList = [];
-    const mostCitiesList = [];
-
-    for (let i = 0; i < states.length; i++) {
-        cityCounterList[i] = [howManyCities(states[i].Sigla), i];
-    }
-
-    // cityCounterList.sort((a, b) => b - a);
-
-    console.log(cityCounterList);
-}
-
-console.log(howManyCities('ES'));
-mostCities();
- */
+init();
