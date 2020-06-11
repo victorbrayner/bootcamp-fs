ec21d07166bee0529dfa898b65871832f194384c
Victor Brayner <victorbrayner@gmail.com>
2020-06-11 16:26:33 -0300

    add POST method

---
+++ modulo-3/my-bank-api/index.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+
+app.use(express.json());
+
+app.post('/account', (req,res) => {
+    let account = req.body;
+
+    fs.readFile('accounts.json', 'utf8', (err, data) => {
+        if (!err) {
+            try {
+                let json = JSON.parse(data);
+
+                account = { id: json.nextID++, ...account };
+                json.accounts.push(account);
+
+                fs.writeFile('accounts.json', JSON.stringify(json), err => {
+                    if (err) {
+                        res.status(400).send({ error: err.message });
+                    } else {
+                        res.end();
+                    }
+                });
+            } catch (err) {
+                res.status(400).send({ error: err.message });
+            }
+        }else{
+            res.status(400).send({ error: err.message });
+        } 
+    });
+});
+
+app.listen(3000, function () {
+    try {
+        fs.readFile('accounts.json', 'utf8', (err, data) => {
+            if(err){
+                const initialJSON = {
+                    nextID: 1,
+                    accounts: []
+                };
+                fs.writeFile('accounts.json', JSON.stringify(initialJSON), (err) => {
+                    res.status(400).send({ error: err.message });
+                });
+            }      
+        });
+        console.log('API Started');
+        
+    } catch (err) {
+        res.status(400).send({ error: err.message });        
+    }
+});
