2ea10d8026f0efa742e3269e6a1a82d7f8aab098
Victor Brayner <victorbrayner@gmail.com>
2020-06-25 14:51:49 -0300

    add shared counters

--- modulo-3/react-counter/src/App.js
+++ modulo-3/react-counter/src/App.js
@@ -1,13 +1,40 @@
 import React, { Component } from "react";
 import Counter from "./components/Counter/Counter";
+import Counter2 from "./components/Counter/Counter2";
 
 export default class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      currentCounter: 3,
+      steps: 0,
+    };
+  }
+
+  handleCount = (clickType) => {
+    const { currentCounter, steps } = this.state;
+
+    this.setState({
+      currentCounter:
+        clickType === "+" ? currentCounter + 1 : currentCounter - 1,
+      steps: steps + 1,
+    });
+  }
+  
+
   render() {
+    const { currentCounter, steps} = this.state;
     return (
       <>
+        <h3>Individual Counter</h3>
         <Counter />
         <Counter />
         <Counter />
+        <h3>Shared Counter</h3>
+        <Counter2 onCount={this.handleCount} countValue ={currentCounter} currentStep={steps}/>
+        <Counter2 onCount={this.handleCount} countValue ={currentCounter} currentStep={steps}/>
+        <Counter2 onCount={this.handleCount} countValue ={currentCounter} currentStep={steps}/>
       </>
     );
   }
