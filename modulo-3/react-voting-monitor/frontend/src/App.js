b76b2065d28341dad0d17523336be6ba6ccb77a5
Victor Brayner <victorbrayner@gmail.com>
2020-06-27 18:17:44 -0300

    add all components

--- modulo-3/react-voting-monitor/frontend/src/App.js
+++ modulo-3/react-voting-monitor/frontend/src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import Header from './components/Header';
+import Preloader from './components/Preloader';
+import Candidates from './components/Candidates';
 
 export default class App extends Component {
   constructor(){
@@ -28,26 +31,13 @@ export default class App extends Component {
 
     if (candidates.length === 0) {
       return (
-      <div className="preloader-wrapper big active">
-        <div className="spinner-layer spinner-blue-only">
-          <div className="circle-clipper left">
-            <div className="circle"></div>
-          </div><div className="gap-patch">
-            <div className="circle"></div>
-          </div><div className="circle-clipper right">
-            <div className="circle"></div>
-          </div>
-        </div>
-      </div>
+        <Preloader description='Carregando...' />
       )
     }
     return (
       <div>
-        {candidates.map(({id, name, votes}) => {
-          return (
-          <p key={id}>{name} - {votes}</p>
-          )
-        })}
+        <Header>Votação</Header>
+        <Candidates candidates={candidates} />
       </div>
       )
   }
---
+++ modulo-3/react-voting-monitor/frontend/src/components/Candidate.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Position from './Position';
+import Picture from './Picture';
+import Info from './Info';
+import Name from './Name';
+import Votes from './Votes';
+import Percentage from './Percentage';
+import Popularity from './Popularity';
+
+
+export default function Candidate({candidate, position}) {
+    const { id, name, votes, percentage, popularity } = candidate;
+    const imageSource = `${id}.jpg`;
+    return (
+        <div>
+            <Position>{position}</Position>
+            <Picture imageSource={imageSource}/>
+            <Info>
+                <Name>{name}</Name>
+                <Votes>{votes}</Votes>
+                <Percentage>{percentage}</Percentage>
+                <Popularity value={popularity} />
+            </Info>
+        </div>
+    )
+}
+ 
